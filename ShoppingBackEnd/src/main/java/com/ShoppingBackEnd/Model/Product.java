39c8f038b26082d47af5e735356ee871b19fbe20
DikpalSingh <singhdk1032@gmail.com>
2020-06-21 09:45:23 +0530

    Changed in model properties.

--- ShoppingBackEnd/src/main/java/com/ShoppingBackEnd/Model/Product.java
+++ ShoppingBackEnd/src/main/java/com/ShoppingBackEnd/Model/Product.java
@@ -1,21 +1,10 @@
 package com.ShoppingBackEnd.Model;
 
+import lombok.Data;
+
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import lombok.Data;
 
 @SuppressWarnings("ALL")
 @Entity
@@ -23,44 +12,40 @@ import lombok.Data;
 @Table(name = "Product")
 public class Product {
 
-	public Product() {
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id", unique = true, nullable = false)
-	private Long id;
+    @Column(name = "product", nullable = false)
+    private String product;
 
-	@Column(name = "product", nullable = false)
-	private String product;
+    @Column(name = "price", nullable = false)
+    private String price;
 
-	@Column(name = "price", nullable = false)
-	private String price;
+    @Column(name = "description")
+    private String desc;
 
-	@Column(name = "description")
-	private String desc;
+    @Column(name = "availableQuantity")
+    private int availableQuantity;
 
-	@Column(name = "availableQuantity")
-	private int availableQuantity;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "prod_id", referencedColumnName = "id")
+    private Set<ProductAttribute> prodAttributeList = new HashSet<>();
 
-	@OneToMany(cascade = CascadeType.ALL)
-	@JoinColumn(name = "prod_id", referencedColumnName = "id")
-	private Set<ProductAttribute> prodAttributeList = new HashSet<>();
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "prod_id", referencedColumnName = "id")
+    private Set<prodImages> prodImagesList = new HashSet<>();
 
-	@OneToMany(cascade = CascadeType.ALL)
-	@JoinColumn(name = "prod_id", referencedColumnName = "id")
-	private Set<prodImages> prodImagesList = new HashSet<>();
+    @Column(name = "sec_id")
+    private Long sec_id;
 
-	@JsonIgnore
-	@Column(name = "sec_id")
-	private Long sec_id;
+    @Column(name = "cat_id")
+    private Long cat_id;
 
-	@JsonIgnore
-	@Column(name = "cat_id")
-	private Long cat_id;
+    @Column(name = "brand_id")
+    private Long brand_id;
 
-	@JsonIgnore
-	@Column(name = "brand_id")
-	private Long brand_id;
-
+    public Product() {
+    }
 }
